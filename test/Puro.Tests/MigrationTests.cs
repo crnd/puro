b0efba5e73998f7689d4a44273998b4805571319
Henri Nieminen <henri@purkki.fi>
2023-12-28 01:19:49 +0200

    Add tests for migration base class

--- test/Puro.Tests/MigrationTests.cs
+++ test/Puro.Tests/MigrationTests.cs
@@ -1,4 +1,9 @@
 ﻿using Puro.Exceptions;
+using Puro.Statements.Create.PrimaryKey;
+using Puro.Statements.Drop.Constraint;
+using Puro.Statements.Drop.Index;
+using Puro.Statements.Drop.Table;
+using Puro.Statements.Sql;
 using Xunit;
 
 namespace Puro.Tests;
@@ -13,6 +18,14 @@ public class MigrationTests
 		var exception = Assert.Throws<NameAttributeNotFoundException>(() => migration.Name);
 		Assert.NotNull(exception);
 		Assert.StartsWith("Migration NamelessMigration ", exception.Message);
+	}
+
+	[Fact]
+	public void EmptyCollectionReturnedFromMigrationWithNoStatements()
+	{
+		var migration = new NamelessMigration();
+
+		Assert.Empty(migration.Statements);
 	}
 
 	private sealed class NamelessMigration : UpMigration
@@ -35,10 +48,99 @@ public class MigrationTests
 	}
 
 	[Fact]
-	public void EmptyCollectionReturnedFromMigrationWithNoStatements()
+	public void StatementsReturnedInCreationOrder()
 	{
-		var migration = new NamelessMigration();
+		var migration = new StatementsMigration();
+		migration.Up();
+
+		Assert.StrictEqual(5, migration.Statements.Count);
+		Assert.IsAssignableFrom<IDropConstraintMigrationStatement>(migration.Statements[0]);
+		Assert.IsAssignableFrom<ICreatePrimaryKeyMigrationStatement>(migration.Statements[1]);
+		Assert.IsAssignableFrom<IDropIndexMigrationStatement>(migration.Statements[2]);
+		Assert.IsAssignableFrom<ISqlMigrationStatement>(migration.Statements[3]);
+		Assert.IsAssignableFrom<IDropTableMigrationStatement>(migration.Statements[4]);
+	}
+
+	[Fact]
+	public void UpOnlyMigrationAddsNoStatementsToDown()
+	{
+		var migration = new StatementsMigration();
+		migration.Down();
 
 		Assert.Empty(migration.Statements);
+	}
+
+	private sealed class StatementsMigration : UpMigration
+	{
+		public override void Up()
+		{
+			Drop.Constraint("PK_Account_CustomerId")
+				.FromTable("Account")
+				.InSchema("Banking");
+
+			Create.PrimaryKey("PK_Account_Id")
+				.OnTable("Account")
+				.InSchema("Banking")
+				.WithColumn("Id");
+
+			Drop.Index("UIX_Account_CustomerId")
+				.FromTable("Accout")
+				.InSchema("Banking");
+
+			Sql("SELECT [AccountNumber] FROM [Banking].[Account];");
+
+			Drop.Table("Account")
+				.InSchema("Banking");
+		}
+	}
+
+	[Fact]
+	public void DownStatementsNotMixedWithUpStatements()
+	{
+		var migration = new TwoWayMigration();
+		migration.Up();
+
+		Assert.StrictEqual(2, migration.Statements.Count);
+		Assert.IsAssignableFrom<ISqlMigrationStatement>(migration.Statements[0]);
+		Assert.IsAssignableFrom<IDropTableMigrationStatement>(migration.Statements[1]);
+	}
+
+	[Fact]
+	public void UpStatementsNotMixedWithDownStatements()
+	{
+		var migration = new TwoWayMigration();
+		migration.Down();
+
+		Assert.StrictEqual(3, migration.Statements.Count);
+		Assert.IsAssignableFrom<IDropConstraintMigrationStatement>(migration.Statements[0]);
+		Assert.IsAssignableFrom<ICreatePrimaryKeyMigrationStatement>(migration.Statements[1]);
+		Assert.IsAssignableFrom<IDropIndexMigrationStatement>(migration.Statements[2]);
+	}
+
+	private class TwoWayMigration : Migration
+	{
+		public override void Up()
+		{
+			Sql("SELECT * FROM [dbo].[Test];");
+
+			Drop.Table("Test")
+				.InSchema("dbo");
+		}
+
+		public override void Down()
+		{
+			Drop.Constraint("PK_Test_TestId")
+				.FromTable("Test")
+				.InSchema("dbo");
+
+			Create.PrimaryKey("PK_Test_Id")
+				.OnTable("Test")
+				.InSchema("dbo")
+				.WithColumn("Id");
+
+			Drop.Index("UIX_Test_Id")
+				.FromTable("Test")
+				.InSchema("dbo");
+		}
 	}
 }
