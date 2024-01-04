02cdb8d55e328b40f59da7759a59a9e8d9ca7df6
Henri Nieminen <henri@purkki.fi>
2024-01-05 00:59:39 +0200

    Add missing comments to ICreateForeignKeyStatement

--- src/Puro/Statements/Create/ForeignKey/ICreateForeignKeyStatement.cs
+++ src/Puro/Statements/Create/ForeignKey/ICreateForeignKeyStatement.cs
@@ -1,14 +1,14 @@
 ﻿namespace Puro.Statements.Create.ForeignKey;
 
 /// <summary>
-/// 
+/// Methods for defining the referencing table.
 /// </summary>
 public interface ICreateForeignKeyStatement
 {
 	/// <summary>
-	/// 
+	/// Defines the referencing table for the foreign key constraint.
 	/// </summary>
 	/// <param name="name">Name of the table.</param>
-	/// <returns></returns>
+	/// <returns>Interface for defining the schema for the referencing table.</returns>
 	public ICreateForeignKeyReferencingTableStatement FromTable(string name);
 }
