4f7b41ef7df548db6922de279f8b7d96370829fa
Henri Nieminen <henri@purkki.fi>
2024-01-21 20:51:05 +0100

    Add test project for SQL Server CLI project

---
+++ test/Puro.SqlServer.CLI.Tests/ExceptionTests.cs
@@ -0,0 +1,19 @@
+using Puro.Exceptions;
+using Xunit;
+
+namespace Puro.SqlServer.CLI.Tests;
+
+public class ExceptionTests
+{
+	[Fact]
+	public void AllExceptionsBasedOnPuroException()
+	{
+		var exceptions = typeof(Program)
+			.Assembly
+			.GetTypes()
+			.Where(t => typeof(Exception).IsAssignableFrom(t))
+			.ToArray();
+
+		Assert.All(exceptions, ex => Assert.True(ex.IsAssignableTo(typeof(PuroException))));
+	}
+}
