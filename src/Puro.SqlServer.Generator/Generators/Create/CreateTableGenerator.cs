d6a192990f2de14d5a35209e84b59074b4b1ffaf
Henri Nieminen <henri@purkki.fi>
2024-01-22 03:01:31 +0100

    Move column T-SQL generation logic to generators level
    New columns will be created within create table and alter table
    statements so move the column generation logic to a level in directory
    structure where both alter and create table statements can use it.

--- src/Puro.SqlServer.Generator/Generators/Create/CreateTableGenerator.cs
+++ src/Puro.SqlServer.Generator/Generators/Create/CreateTableGenerator.cs
@@ -31,7 +31,7 @@ internal static class CreateTableGenerator
 		var builder = new StringBuilder($"CREATE TABLE [{statement.Schema}].[{statement.Table}] (").AppendLine();
 
 		var columns = statement.Columns
-			.Select(BuildColumnRowSql)
+			.Select(ColumnGenerator.BuildColumnRowSql)
 			.ToList();
 
 		builder.Append(string.Join("," + Environment.NewLine, columns));
@@ -46,7 +46,7 @@ internal static class CreateTableGenerator
 			return false;
 		}
 
-		if (statement.Columns.Any(c => !ColumnIsComplete(c)))
+		if (statement.Columns.Any(c => !ColumnGenerator.ColumnIsComplete(c)))
 		{
 			return false;
 		}
@@ -59,104 +59,5 @@ internal static class CreateTableGenerator
 		}
 
 		return true;
-	}
-
-	private static bool ColumnIsComplete(ITableColumn column)
-	{
-		if (column.Type is null || column.Nullable is null)
-		{
-			return false;
-		}
-
-		if (column.Type == typeof(decimal) && (column.Precision is null || column.Scale is null))
-		{
-			return false;
-		}
-
-		return true;
-	}
-
-	private static string BuildColumnRowSql(ITableColumn column)
-	{
-		var sql = $"[{column.Name}] {ColumnToSqlType(column)} ";
-
-		if (!column.Nullable!.Value)
-		{
-			sql += "NOT ";
-		}
-
-		sql += "NULL";
-
-		if (column.Identity)
-		{
-			sql += " IDENTITY(1, 1)";
-		}
-
-		return sql;
-	}
-
-	private static string ColumnToSqlType(ITableColumn tableColumn)
-	{
-		return tableColumn switch
-		{
-			var column when column.Type == typeof(bool) => "BIT",
-			var column when column.Type == typeof(short) => "SMALLINT",
-			var column when column.Type == typeof(int) => "INT",
-			var column when column.Type == typeof(long) => "BIGINT",
-			var column when column.Type == typeof(double) => "FLOAT(53)",
-			var column when column.Type == typeof(decimal) => BuildDecimalType(column.Precision!.Value, column.Scale!.Value),
-			var column when column.Type == typeof(string) => BuildStringType(column.FixedLength, column.MaximumLength),
-			var column when column.Type == typeof(Guid) => "UNIQUEIDENTIFIER",
-			var column when column.Type == typeof(DateOnly) => "DATE",
-			var column when column.Type == typeof(TimeOnly) => "TIME",
-			var column when column.Type == typeof(DateTime) => "DATETIME",
-			var column when column.Type == typeof(DateTimeOffset) => "DATETIMEOFFSET",
-			_ => throw new NotImplementedException()
-		};
-	}
-
-	private static string BuildDecimalType(int precision, int scale)
-	{
-		if (precision < 1 || precision > 38)
-		{
-			throw new InvalidDecimalPrecisionException(precision);
-		}
-
-		if (scale < 0)
-		{
-			throw new InvalidDecimalScaleException(scale);
-		}
-
-		if (scale > precision)
-		{
-			throw new InvalidDecimalScaleException(scale, precision);
-		}
-
-		return $"DECIMAL({precision}, {scale})";
-	}
-
-	private static string BuildStringType(int? fixedLength, int? maximumLength)
-	{
-		if (fixedLength is not null)
-		{
-			if (fixedLength.Value > 4000)
-			{
-				throw new InvalidStringLengthException(fixedLength.Value);
-			}
-
-			return $"NCHAR({fixedLength.Value})";
-		}
-
-		if (maximumLength is not null)
-		{
-			if (maximumLength.Value > 4000)
-			{
-				throw new InvalidStringLengthException(maximumLength.Value);
-			}
-
-			return $"NVARCHAR({maximumLength})";
-		}
-
-		return "NVARCHAR(MAX)";
 	}
 }
