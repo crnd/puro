49e30b7bf9661ea75c492c977f7e65fc32dac8c7
Henri Nieminen <henri@purkki.fi>
2024-09-29 21:32:44 +0200

    Add tests for migration SQL generator

--- test/Puro.SqlServer.Runner.Tests/Generators/MigrationSqlGeneratorTests.cs
+++ test/Puro.SqlServer.Runner.Tests/Generators/MigrationSqlGeneratorTests.cs
@@ -15,4 +15,112 @@ public class MigrationSqlGeneratorTests
 
 		expected.SqlEqual(sql);
 	}
+
+	[Fact]
+	public void SingleMigration()
+	{
+		var migration = new CreateTableMigration();
+		migration.Up();
+
+		var sql = MigrationSqlGenerator.Generate([migration]);
+
+		const string expected = $"""
+			{Constants.MigrationTableCreation}
+			BEGIN TRANSACTION;
+
+			IF NOT EXISTS (SELECT 1 FROM [dbo].[__PuroMigrationsHistory] WHERE [MigrationId] = N'1_CreateTable')
+			BEGIN
+
+			CREATE TABLE [dbo].[Book] (
+				[Id] INT NOT NULL IDENTITY(1, 1),
+				[Name] NVARCHAR(MAX) NOT NULL
+			);
+
+			ALTER TABLE [dbo].[Book]
+				ADD CONSTRAINT [PK_Book] PRIMARY KEY CLUSTERED ([Id]);
+
+			INSERT INTO [dbo].[__PuroMigrationsHistory] ([MigrationName], [AppliedOn])
+			VALUES (N'1_CreateTable', SYSUTCDATETIME()));
+
+			END
+
+			COMMIT TRANSACTION;
+			""";
+
+		expected.SqlEqual(sql);
+	}
+
+	[Fact]
+	public void MultipleMigrations()
+	{
+		var migration1 = new CreateTableMigration();
+		migration1.Up();
+		var migration2 = new CreateIndexMigration();
+		migration2.Up();
+
+		var sql = MigrationSqlGenerator.Generate([migration1, migration2]);
+
+		const string expected = $"""
+			{Constants.MigrationTableCreation}
+			BEGIN TRANSACTION;
+			
+			IF NOT EXISTS (SELECT 1 FROM [dbo].[__PuroMigrationsHistory] WHERE [MigrationId] = N'1_CreateTable')
+			BEGIN
+			
+			CREATE TABLE [dbo].[Book] (
+				[Id] INT NOT NULL IDENTITY(1, 1),
+				[Name] NVARCHAR(MAX) NOT NULL
+			);
+			
+			ALTER TABLE [dbo].[Book]
+				ADD CONSTRAINT [PK_Book] PRIMARY KEY CLUSTERED ([Id]);
+			
+			INSERT INTO [dbo].[__PuroMigrationsHistory] ([MigrationName], [AppliedOn])
+			VALUES (N'1_CreateTable', SYSUTCDATETIME()));
+			
+			END
+
+			IF NOT EXISTS (SELECT 1 FROM [dbo].[__PuroMigrationsHistory] WHERE [MigrationId] = N'2_CreateIndex')
+			BEGIN
+
+			CREATE UNIQUE INDEX [UIX_Book_Name]
+				ON [dbo].[Book] ([Name] ASC);
+
+			INSERT INTO [dbo].[__PuroMigrationsHistory] ([MigrationName], [AppliedOn])
+			VALUES (N'2_CreateIndex', SYSUTCDATETIME()));
+
+			END
+			
+			COMMIT TRANSACTION;
+			""";
+
+		expected.SqlEqual(sql);
+	}
+
+	[MigrationName("1_CreateTable")]
+	private sealed class CreateTableMigration : UpMigration
+	{
+		public override void Up()
+		{
+			Create.Table("Book")
+				.WithColumn("Id").AsInt().Identity()
+				.WithColumn("Name").AsString().NotNull();
+
+			Create.PrimaryKey("PK_Book")
+				.OnTable("Book")
+				.WithColumn("Id");
+		}
+	}
+
+	[MigrationName("2_CreateIndex")]
+	private sealed class CreateIndexMigration : UpMigration
+	{
+		public override void Up()
+		{
+			Create.UniqueIndex("UIX_Book_Name")
+				.OnTable("Book")
+				.OnColumn("Name")
+				.Ascending();
+		}
+	}
 }
